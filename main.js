b18f50af7f596ccf097b8f6517fc57bedaf38fb5
Fengyuan Chen <cfy1990@gmail.com>
2015-09-15 17:15:16 +0800

    use angularjs to draw board

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 /**
  * Created by chenfengyuan on 9/15/15.
  */
-(function($){
-    console.log($);
-    var container = $('#board');
-    console.log(container);
-    for (var i = 0; i < 100; i++) {
-        container.append('<div class="square" id="square-' + i + '"><div class="inner-square"><div class="triangle triange-red"></div></div></div>');
-	}
-})($);
+angular.module('main', [])
+  .controller('board', function() {
+        this.squares=[];
+        this.colors=["red", "pink", "blue", "gray"];
+        this.max_type=4;
+        this.current_color=this.colors[0];
+        this.square_click = function(square){
+            square.color = this.current_color;
+            if(square.type == 2 || square.type == 3){
+                square.dir = (square.dir + 1)% 4;
+                if(square.dir == 0){
+                    square.type = (square.type + 1)% this.max_type;
+                }
+            }else{
+                square.type = (square.type + 1) % this.max_type;
+            }
+        };
+        var i;
+        this.dir_css_map=["triangle-up", "triangle-right", "triangle-down", "triangle-left"];
+        this.arrow_dir_css_map=["black-triangle-up", "black-triangle-right", "black-triangle-down", "black-triangle-left"];
+        for(i=0;i<100;i++){
+            this.squares.push({type:0, dir:0, color:"red"});
+        }
+        console.log(this.squares);
+    });
